2b265c279ff123480f2f3fe95ce756dbc7f61c7b
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-03-09 19:44:50 +0000

    feat: added an imagepath type to Artwork types

--- src/types/artwork.ts
+++ src/types/artwork.ts
@@ -5,4 +5,5 @@ export type Artwork = {
   type: string;
   price: number;
   availability: boolean;
+  imagepath?: string; // "?"-необов’язкове поле, Якщо бекенд повертає null або запис був доданий без фото, то imagePath просто не буде у об’єкті.
 };
